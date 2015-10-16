156f9dc632674e1cb3b3a4b49c4d9fc1ec49297a
73hi3x <mercues@gmail.com>
2015-10-16 22:28:47 +0200

    classe element et personne

--- src/listeObjetsChainees/Element.java
+++ src/listeObjetsChainees/Element.java
@@ -1,11 +1,40 @@
 package listeObjetsChainees;
 
+/**
+ * chainés des éléments  entre eux.
+ * @author 73hi3x
+ *
+ */
 public class Element {
+
+	private Personne personne;
+	private Element next;
 	
-	void Element(){
-		Personne objet;
-		Element element;
+	public Personne getPersonne() {
+		return personne;
+	}
+	public void setPersonne(Personne personne) {
+		this.personne = personne;
+	}
+	public Element getNext() {
+		return next;
+	}
+	public void setNext(Element next) {
+		this.next = next;
 	}
 
+	public Element() {
+		super();
+	}
 
+	public Element(Personne personne, Element next) {
+		super();
+		this.personne = personne;
+		this.next = next;
+	}
+	@Override
+	public String toString() {
+		return personne + " " + next;
+	}
+	 
 }
--- src/listeObjetsChainees/Personne.java
+++ src/listeObjetsChainees/Personne.java
@@ -1,18 +1,38 @@
 package listeObjetsChainees;
+/**
+ * 
+ * classe Personne
+ * @author 73hi3x
+ *
+ */
 
-public class Personne {
-	  private String nom;
-	  private String prenom;
+	  public class Personne {
+		  private String nom;
+		  private String prenom;
+		  public String getNom() {
+		  	return nom;
+		  }
+		  public void setNom(String nom) {
+		  	this.nom = nom;
+		  }
+		  public String getPrenom() {
+		  	return prenom;
+		  }
+		  public void setPrenom(String prenom) {
+		  	this.prenom = prenom;
+		  }
+		  public Personne() {
+		  	super();
+		  }
+		  public Personne(String nom, String prenom) {
+		  	super();
+		  	this.nom = nom;
+		  	this.prenom = prenom;
+		  }
+		  @Override
+		  public String toString() {
+		  	return  nom + " " + prenom;
+		  }
 
-	  Personne(String nom, String prenom)
-	  {
-	  this.nom= nom;
-	  this.prenom=prenom;
-	  }
+		  }
 
-	  public String toString()
-	  {return "nom : " + nom +", prenom : " +prenom;}
-	  
-
-
-}
